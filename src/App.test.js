3241379414d7239040a2974fcc342d8583c6adb4
rcrossa <rcrossa@hotmail.com>
2021-10-31 15:53:21 -0300

    Fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import TestBasico from './components/TestBasico/TestBasico';
 
 test(` ${TestBasico}`, () => {
-  render(<App />);
+  render(<App/>);
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 });
